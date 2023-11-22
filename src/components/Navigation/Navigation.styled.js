bc814e3d94215ac555e017facb9a54b127dbd174
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-11-23 01:18:21 +0200

    Update Navigation.styled.js

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -6,9 +6,27 @@ export const Wrapper = styled.div`
   gap: 35px;
 `;
 export const StyledNavlink = styled(NavLink)`
+  position: relative;
   padding: 24px 0;
   color: black;
   &:hover {
     color: rosybrown;
+  }
+  &.active {
+    color: rosybrown;
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -1px;
+
+      display: block;
+      width: 100%;
+      height: 4px;
+
+      background-color: rosybrown;
+      border-radius: 2px;
+      border: 1px solid black;
+    }
   }
 `;
