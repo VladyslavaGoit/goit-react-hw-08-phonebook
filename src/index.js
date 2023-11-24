d6361feeb5703fd0265932b1910d8f286cd04540
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-11-24 20:57:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App/App';
+
 import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
