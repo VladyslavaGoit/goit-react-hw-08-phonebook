bf0e9cab1247b08e523185215aa04c0530fc8016
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-12-13 21:47:44 +0200

    adds error and isLoading to state about auth

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,6 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { logIn, logOut, refreshUser, register } from './operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, action) => {
+  state.error = action.payload;
+  state.isLoading = false;
+};
+
 const initialState = {
   user: {
     name: null,
@@ -9,27 +19,38 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  isLoading: false,
+  error: null,
 };
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
+    [register.pending]: handlePending,
     [register.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
+    [register.rejected]: handleRejected,
+
+    [logOut.pending]: handlePending,
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
     },
+    [logOut.rejected]: handleRejected,
+
+    [logIn.pending]: handlePending,
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
+    [logIn.rejected]: handleRejected,
+
     [refreshUser.pending](state) {
       state.isRefreshing = true;
     },
